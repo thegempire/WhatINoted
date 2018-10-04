4c46fd379fa3badb0bec4e6ab43d2f9835dc3619
Ian Younghusband <ianyounghusband@hotmail.com>
2018-10-04 16:02:50 -0700

    began configuring search method

--- WhatINoted/AddNotebook.aspx.designer.cs
+++ WhatINoted/AddNotebook.aspx.designer.cs
@@ -20,5 +20,23 @@ namespace WhatINoted {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.UpdatePanel AddNotebookUpdatePanel;
+        
+        /// <summary>
+        /// SearchGridISBN control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl SearchGridISBN;
+        
+        /// <summary>
+        /// SearchGridDetails control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl SearchGridDetails;
     }
 }
