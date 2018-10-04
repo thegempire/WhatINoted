b9f3176acba8d05baf3218e672e92158a85cd1fd
paulszefer <pszefer@hotmail.com>
2018-10-03 21:51:09 -0700

    Added Create Note page to allow the user to create a new note

---
+++ WhatINoted/AddNote.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WhatINoted
+{
+    public partial class AddNote : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ByImageGroupContainer.Visible = false;
+        }
+
+        protected void ByImage_Click(object sender, EventArgs e)
+        {
+            ByImageGroupContainer.Visible = !ByImageGroupContainer.Visible;
+        }
+    }
+}
