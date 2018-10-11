93da3b7b3a660c8dadc053452948a6fa179eeabf
Liam MacIntyre <liam.macintyre@gmail.com>
2018-10-11 14:07:05 -0700

    added comments

--- WhatINoted/ConnectionManagers/GoogleBooksConnectionManager.cs
+++ WhatINoted/ConnectionManagers/GoogleBooksConnectionManager.cs
@@ -6,9 +6,19 @@ using System.Drawing;
 
 namespace WhatINoted.ConnectionManagers
 {
+    /// <summary>
+    /// Connection Manager to handle interactions with the Google Books API.
+    /// Specifically this class is used to search for book details in Google's
+    /// database of titles.
+    /// </summary>
     public class GoogleBooksConnectionManager
     {
-        private static readonly String RedirectURI = ""; // TODO Get a redirect URI for Google Books. Maybe read this from a config file?
+        private static readonly String API_KEY = ""; // TODO Get an API key for Google Books. Maybe read this from a config file? 
+
+        /// <summary>
+        /// Redirect URI to which Google Books will send responses.
+        /// </summary>
+        private static readonly String RedirectURI = ""; // TODO Set up a redirect URI for Google Books. Maybe read this from a config file?
 
         // TODO return BookSearchResults
         // TODO parameter: IsbnModel
--- WhatINoted/ConnectionManagers/GoogleVisionConnectionManager.cs
+++ WhatINoted/ConnectionManagers/GoogleVisionConnectionManager.cs
@@ -6,10 +6,23 @@ using System.Drawing;
 
 namespace WhatINoted.ConnectionManagers
 {
+    /// <summary>
+    /// Connection Manager to handle interactions with the Google Vision image
+    /// analysis API. Specifically this class is used to connect to the API
+    /// and extract text from a given image.
+    /// </summary>
     public class GoogleVisionConnectionManager
     {
-        private static readonly String ApiKey = ""; // TODO -- Get an API key. Either put it here, or read it from a config file?
+        /// <summary>
+        /// API key to access Google Vision.
+        /// </summary>
+        private static readonly String API_KEY = ""; // TODO -- Get an API key. Either put it here, or read it from a config file?
 
+        /// <summary>
+        /// Send a request to Google Vision to extract extract text from an image.
+        /// </summary>
+        /// <param name="image">the image to analyze</param>
+        /// <returns>A string extracted from the image, or null if the call failed.</returns>
         public static String ExtractText(Image image)
         {
             // TODO: OCR API interaction
