0fc9041d3076ce9194e730c716e9d1f27d676f96
Liam MacIntyre <liam.macintyre@gmail.com>
2018-10-11 11:11:15 -0700

    created empty model classes

---
+++ WhatINoted/Models/UserModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhatINoted.Models
+{
+    /// <summary>
+    /// UserModel: Stores user information
+    /// </summary>
+    public class UserModel
+    {
+        /// <summary>
+        /// User's display name
+        /// </summary>
+        private readonly String Name;
+
+        /// <summary>
+        /// User ID
+        /// </summary>
+        private readonly String Uid;
+
+        // TODO: Add List<NotebookModel>
+
+        /// <summary>
+        /// Construct a UserModel given a display name and uid.
+        /// 
+        /// </summary>
+        /// <param name="Name">display name</param>
+        /// <param name="Uid">uid</param>
+        public UserModel(String Name, String Uid)
+        {
+            this.Name = Name;
+            this.Uid = Uid;
+        }
+
+        public String GetName()
+        {
+            return Name;
+        }
+
+        public String GetUid()
+        {
+            return Uid;
+        }
+    }
+}
