4f1958823614cbb3c714180855a378de9e32ae99
Liam MacIntyre <liam.macintyre@gmail.com>
2018-10-11 13:53:59 -0700

    created books and vision connection managers

---
+++ WhatINoted/ConnectionManagers/GoogleBooksConnectionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+namespace WhatINoted.ConnectionManagers
+{
+    public class GoogleBooksConnectionManager
+    {
+        private static readonly String RedirectURI = ""; // TODO Get a redirect URI for Google Books. Maybe read this from a config file?
+
+        // TODO return BookSearchResults
+        // TODO parameter: IsbnModel
+        public static void SearchByISBN()
+        {
+
+        }
+
+        // TODO return BookSearchResults
+        public static void SearchByDetails(String title, String author)
+        {
+
+        }
+    }
+}
---
+++ WhatINoted/ConnectionManagers/GoogleVisionConnectionManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+namespace WhatINoted.ConnectionManagers
+{
+    public class GoogleVisionConnectionManager
+    {
+        private static readonly String ApiKey = ""; // TODO -- Get an API key. Either put it here, or read it from a config file?
+
+        public static String ExtractText(Image image)
+        {
+            // TODO: OCR API interaction
+
+            return null;
+        }
+    }
+}
