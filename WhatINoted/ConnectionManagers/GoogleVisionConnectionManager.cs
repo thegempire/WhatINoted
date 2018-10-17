b1cf0b8d0afd6b118a61ab2b18f12c971ead7861
Ian Younghusband <ianyounghusband@hotmail.com>
2018-10-17 13:44:54 -0700

    Made changes for testing the OCR connection api

--- WhatINoted/ConnectionManagers/GoogleVisionConnectionManager.cs
+++ WhatINoted/ConnectionManagers/GoogleVisionConnectionManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Drawing;
+using Google.Cloud.Vision.V1;
+using Image = Google.Cloud.Vision.V1.Image;
+using System.IO;
 
 namespace WhatINoted.ConnectionManagers
 {
@@ -16,18 +19,32 @@ namespace WhatINoted.ConnectionManagers
         /// <summary>
         /// API key to access Google Vision.
         /// </summary>
-        private static readonly String API_KEY = ""; // TODO -- Get an API key. Either put it here, or read it from a config file?
+        private static readonly string API_KEY = ""; // TODO -- Get an API key. Either put it here, or read it from a config file?
 
         /// <summary>
         /// Send a request to Google Vision to extract extract text from an image.
         /// </summary>
         /// <param name="image">the image to analyze</param>
         /// <returns>A string extracted from the image, or null if the call failed.</returns>
-        public static String ExtractText(Image image)
+        public static string ExtractText(System.Drawing.Image originalImage)
         {
-            // TODO: OCR API interaction
+            //Convert originalImage to bytes
+            byte[] imageBytes = ImageToByteArray(originalImage);
 
-            return null;
+            Image image = Image.FromBytes(imageBytes);
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "C:\\Users\\Ian\\source\\repos\\WhatINoted\\WhatINoted-5bba0c1ecaf8.json");
+            ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+            IReadOnlyList<EntityAnnotation> textAnnotations = client.DetectText(image);
+            return textAnnotations.First().Description;
+        }
+
+        public static byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, imageIn.RawFormat);
+                return ms.ToArray();
+            }
         }
     }
 }
