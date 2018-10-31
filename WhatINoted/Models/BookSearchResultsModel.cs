3749f4eb2a4a711cc6fb94130e8a6dfe844942d8
Ian Younghusband <ianyounghusband@hotmail.com>
2018-10-31 10:57:26 -0700

    Created tests for GoogleBooks and IsbnModel - To be used once implementation is done.

--- WhatINoted/Models/BookSearchResultsModel.cs
+++ WhatINoted/Models/BookSearchResultsModel.cs
@@ -16,9 +16,14 @@ namespace WhatINoted.Models
         public readonly String Title;
 
         /// <summary>
-        /// Author of the book.
+        /// Authors of the book.
         /// </summary>
-        public readonly String Author;
+        public readonly String Authors;
+
+        /// <summary>
+        /// Publisher of the book.
+        /// </summary>
+        public readonly String Publisher;
 
         /// <summary>
         /// ISBN of the book.
@@ -26,16 +31,32 @@ namespace WhatINoted.Models
         public readonly IsbnModel ISBN;
 
         /// <summary>
+        /// Publish Date of the book.
+        /// </summary>
+        public readonly DateTime PublishDate;
+
+        /// <summary>
+        /// Cover URL of the book.
+        /// </summary>
+        public readonly String CoverURL;
+
+        /// <summary>
         /// Construct a BookSearchResultsModel with the given characteristics.
         /// </summary>
         /// <param name="Title">title of the book</param>
-        /// <param name="Author">author of the book</param>
+        /// <param name="Authors">author of the book</param>
+        /// <param name="Publisher">publisher of the book</param>
         /// <param name="ISBN">isbn of the book</param>
-        public BookSearchResultsModel(String Title, String Author, IsbnModel ISBN)
+        /// <param name="PublishDate">publish date of the book</param>
+        /// <param name="CoverURL">cover url of the book</param>
+        public BookSearchResultsModel(String Title, String Authors, String Publisher, IsbnModel ISBN, DateTime PublishDate, String CoverURL)
         {
             this.Title = Title;
-            this.Author = Author;
+            this.Authors = Authors;
+            this.Publisher = Publisher;
             this.ISBN = ISBN;
+            this.PublishDate = PublishDate;
+            this.CoverURL = CoverURL;
         }
     }
 }
