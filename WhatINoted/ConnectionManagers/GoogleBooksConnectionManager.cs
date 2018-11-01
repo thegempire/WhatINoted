1cc6892b8b0d28db88c50a6e578b455cabd56189
Liam MacIntyre <liam.macintyre@gmail.com>
2018-11-01 09:41:53 -0700

    isbn validation, needs exceptions

--- WhatINoted/ConnectionManagers/GoogleBooksConnectionManager.cs
+++ WhatINoted/ConnectionManagers/GoogleBooksConnectionManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Drawing;
+using Newtonsoft.Json;
 
 namespace WhatINoted.ConnectionManagers
 {
@@ -13,12 +14,7 @@ namespace WhatINoted.ConnectionManagers
     /// </summary>
     public class GoogleBooksConnectionManager
     {
-        private static readonly String API_KEY = ""; // TODO Get an API key for Google Books. Maybe read this from a config file? 
-
-        /// <summary>
-        /// Redirect URI to which Google Books will send responses.
-        /// </summary>
-        private static readonly String RedirectURI = ""; // TODO Set up a redirect URI for Google Books. Maybe read this from a config file?
+        private static readonly String REQUEST_BASE = "https://www.googleapis.com/books/v1/volumes?q=";
 
         /// <summary>
         /// Searches for books by ISBN via the Google Books API.
@@ -39,6 +35,66 @@ namespace WhatINoted.ConnectionManagers
         public static List<Models.BookSearchResultsModel> SearchByDetails(String title, String author)
         {
             return new List<Models.BookSearchResultsModel>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="title">title to search for</param>
+        /// <param name="author">author to search for</param>
+        /// <param name="publisher">publisher to search for</param>
+        /// <param name="isbn">isbn to search for</param>
+        /// <returns></returns>
+        private static String BuildRequestURI(String title, String author, String publisher, String isbn)
+        {
+            bool append = false;
+            String requestUri = REQUEST_BASE;
+
+            // Append title if not empty
+            if (title != "")
+            {
+                requestUri += "intitle:" + title;
+                append = true;
+            }
+
+            // Append author if not empty
+            if (author != "")
+            {
+                if (append)
+                    requestUri += "+";
+
+                requestUri += "inauthor:" + author;
+                append = true;
+            }
+
+            // Append publisher if not empty
+            if (publisher != "")
+            {
+                if (append)
+                    requestUri += "+";
+
+                requestUri += "inpublisher:" + publisher;
+                append = true;
+            }
+
+            // Append ISBN if not empty
+            if (isbn != "")
+            {
+                if (append)
+                    requestUri += "+";
+
+                requestUri += "isbn:" + isbn;
+                append = true;
+            }
+
+            return requestUri;
+        }
+
+        private static List<Models.BookSearchResultsModel> SearchResultsFromJSON(String jsonString)
+        {
+            var jsonObj = JsonConvert.DeserializeObject(jsonString);
+
+
         }
     }
 }
--- WhatINoted/Models/IsbnModel.cs
+++ WhatINoted/Models/IsbnModel.cs
@@ -5,6 +5,8 @@ using System.Web;
 
 namespace WhatINoted.Models
 {
+    public enum IsbnType { ISBN_10, ISBN_13 };
+
     /// <summary>
     /// IsbnModel stores information representing an ISBN.
     /// </summary>
@@ -15,23 +17,85 @@ namespace WhatINoted.Models
         /// </summary>
         public readonly String Number;
 
+        public readonly IsbnType Type;
+
         /// <summary>
         /// Construct an IsbnModel with the given ISBN.
         /// </summary>
         /// <param name="number">String representation of the ISBN</param>
-        public IsbnModel(String Number)
+        public IsbnModel(String Number, IsbnType Type)
         {
             this.Number = Number;
+            this.Type = Type;
         }
 
         /// <summary>
         /// Validate the ISBN.
         /// </summary>
         /// <returns>true if and only if the ISBN is valid</returns>
-        public bool IsValid()
+        private bool IsValid()
         {
-            // TODO: validation
-            return false;
+            switch(Type)
+            {
+                case IsbnType.ISBN_10:
+                    return isValidIsbn10();
+
+                case IsbnType.ISBN_13:
+                    return isValidIsbn13();
+
+                default:
+                    return false;
+            }
+        }
+
+        private bool isValidIsbn10()
+        {
+            int sum = 0;
+            int digit;
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (Int32.TryParse(Number.Substring(i, i + 1), out digit))
+                    sum += (10 - i) * digit;
+
+                else
+                    throw new ArgumentException("");
+            }
+
+            if (Int32.TryParse(Number.Substring(9, 10), out digit))
+                sum += 10 * digit;
+
+            else if (Number[9] == 'X')
+                sum += 10;
+
+            else
+                ;// throw an exception
+
+            return (sum % 11) == 0;
+        }
+
+        private bool isValidIsbn13()
+        {
+            int sum = 0;
+            int digit;
+
+            for (int i = 0; i < 13; i++)
+            {
+                if (Int32.TryParse(Number.Substring(i, i+1), out digit))
+                {
+                    if (i % 2 == 1)
+                        sum += digit;
+
+                    else
+                        sum += 3 * digit;
+                }
+                else
+                {
+                    // throw an exception
+                }
+            }
+
+            return sum % 10 == 0;
         }
     }
 }
