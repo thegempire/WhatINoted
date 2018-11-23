4cfd077f6363f17298653d485c2fd6cb9c98dd34
paulszefer <pszefer@hotmail.com>
2018-11-23 12:30:30 -0800

    Revert accidental commit of changing debug mode to true

--- WhatINoted/DebugMode.cs
+++ WhatINoted/DebugMode.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace WhatINoted
+﻿namespace WhatINoted
 {
     public class DebugMode
     {
-
         public static readonly bool DEBUG = false; // MUST BE FALSE WHEN YOU DEPLOY
-
     }
 }
